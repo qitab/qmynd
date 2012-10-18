4bdf4ee74d174c08284d819d8b99591702d3ef9b
Alejandro R Sedeño <asedeno@google.com>
2012-10-18 17:43:01 -0400

    text-protocol: implement command-process-information

---
+++ src/mysql-protocol/text-protocol/command-process-information.lisp
@@ -0,0 +1,53 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;                                                                  ;;;
+;;; Free Software published under an MIT-like license. See LICENSE   ;;;
+;;;                                                                  ;;;
+;;; Copyright (c) 2012 Google, Inc.  All rights reserved.            ;;;
+;;;                                                                  ;;;
+;;; Original author: Alejandro Sedeño                                ;;;
+;;;                                                                  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package :mysqlnd)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 15.6.11 command-process-info
+;;; NB: This command is deprecated; use the query "SHOW PROCESSLIST" instead.
+
+;; We don't actually receive this packet as a client, but it looks like this.
+
+;; (define-packet command-process-info
+;;   ((tag :mysql-type (integer 1)
+;;         :value +mysql-command-process-info+
+;;         :transient t :bind nil)))
+
+;; Returns Resultset or ERR packet
+
+(defun send-command-process-information ()
+  (with-mysql-connection (c)
+    (mysql-command-init c +mysql-command-process-information+)
+    (mysql-write-packet (vector +mysql-command-process-information+))
+    (let* ((payload (mysql-read-packet))
+           (tag (aref payload 0)))
+      (case tag
+        ((#.+mysql-response-error+) (parse-response payload))
+        (otherwise
+         (let* ((column-count (parse-column-count payload))
+                (column-definitions (coerce
+                                     (loop
+                                       repeat column-count
+                                       collect (parse-column-definition-v41 (mysql-read-packet))
+                                       ;; asedeno-TODO: do something better here; probably signal for
+                                       ;; ERR in parse-response.
+                                       finally (assert (typep (parse-response (mysql-read-packet))
+                                                              'response-end-of-file-packet)))
+                                     'vector))
+                (rows (parse-resultset-rows column-count column-definitions)))
+           (values rows column-definitions)))))))
+
+#|
+;; If COM_PROCESS_INFO ever gets disabled, compatability stub:
+;; NB: this file will need to depend on command-process-query
+(defun send-command-process-information ()
+  (send-command-query "SHOW PROCESSLIST"))
+|#
